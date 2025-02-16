2a47d682ea6fe4ef71b3bf354f197f918ea6202c
agt1984 <69942981+agt1984@users.noreply.github.com>
2025-02-16 19:41:47 +0100

    Board Game New Functions

--- src/App.java
+++ src/App.java
@@ -12,11 +12,16 @@ public class App {
 
                       // Create and configure the game window
         JFrame frame = new JFrame("Pac-Man");
-        frame.setVisible(true);                    // Make the window visible
+        //frame.setVisible(true);                    // Make the window visible
         frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);         // Center the window on the screen
         frame.setResizable(false);         // Prevent the window from being resized
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        PacMan pacmanGame = new PacMan();
+        frame.add(pacmanGame);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
 
