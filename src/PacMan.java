dfa26fe1307885d94f4f37992d081130d0e95932
agt1984 <69942981+agt1984@users.noreply.github.com>
2025-02-16 19:38:56 +0100

    Board Game New Functions

--- src/PacMan.java
+++ src/PacMan.java
@@ -1,5 +1,21 @@
+import java.awt.Color;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.HashSet;
+import java.util.Random;
 import javax.swing.*;
 
 public class PacMan extends JPanel{
+    private int rowCount = 21;                          // Number of rows
+    private int columnCount = 19;                       // Number of columns
+    private int tileSize = 32;                          // Size of each cell in pixels
+    private int boardWidth = columnCount * tileSize;    // Total width
+    private int boardHeight = rowCount * tileSize;      // Total height
 
+    PacMan(){
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+        setBackground(Color.BLACK);
+    }
 }
+
+
