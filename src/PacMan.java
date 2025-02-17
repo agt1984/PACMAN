3b8d4f0b84b9803029308042e38db629da5927d7
agt1984 <69942981+agt1984@users.noreply.github.com>
2025-02-17 20:54:55 +0100

    String map added

--- src/PacMan.java
+++ src/PacMan.java
@@ -1,6 +1,8 @@
 import java.awt.*;
+import java.util.HashSet;
 import javax.swing.*;
 
+@SuppressWarnings("unused")
 public class PacMan extends JPanel{
     class Block {
         int x;
@@ -33,12 +35,45 @@ public class PacMan extends JPanel{
     private Image blueGhostImage;
     private Image orangeGhostImage;
     private Image pinkGhostImage;
+    @SuppressWarnings("FieldMayBeFinal")
     private Image redGhostImage;
 
+    @SuppressWarnings("FieldMayBeFinal")
     private Image pacmanUpImage;
     private Image pacmanDownImage;
     private Image pacmanLeftImage;
     private Image pacmanRightImage;
+
+    //X = wall, O = skip, P = pac man, ' ' = food
+    //Ghosts: b = blue, o = orange, p = pink, r = red, ee
+    private String[] tileMap = {
+        "XXXXXXXXXXXXXXXXXXX",
+        "X        X        X",
+        "X XX XXX X XXX XX X",
+        "X                 X",
+        "X XX X XXXXX X XX X",
+        "X    X       X    X",
+        "XXXX XXXX XXXX XXXX",
+        "OOOX X       X XOOO",
+        "XXXX X XXrXX X XXXX",
+        "O       bpo       O",
+        "XXXX X XXXXX X XXXX",
+        "OOOX X       X XOOO",
+        "XXXX X XXXXX X XXXX",
+        "X        X        X",
+        "X XX XXX X XXX XX X",
+        "X  X     P     X  X",
+        "XX X X XXXXX X X XX",
+        "X    X   X   X    X",
+        "X XXXXXX X XXXXXX X",
+        "X                 X",
+        "XXXXXXXXXXXXXXXXXXX" 
+    };
+
+    HashSet<Block> walls;
+    HashSet<Block> foods;
+    HashSet<Block> ghosts;
+    Block pacman;
 
     PacMan(){
         setPreferredSize(new Dimension(boardWidth, boardHeight));
